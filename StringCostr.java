e07ac508ae0adce8a0978af29e7753027b1b3cbf
Andretia90 <andryjay90@hotmail.it>
2023-07-12 13:01:42 +0200

    ultimo ese avanzato

---
+++ StringCostr.java
@@ -0,0 +1,21 @@
+package javaOopAdvanced.AdvancedIDEfeaturesDebugger;
+
+public class StringCostr {
+    public static void main(String[] args) {
+
+
+
+    {
+
+        /*
+         * In Java, StringBuilderè una classe che fornisce un modo più efficiente per manipolare le stringhe
+         *  che richiedono modifiche frequenti. A differenza della Stringclasse, che è immutabile e non può essere
+         *  cambiata una volta creata, StringBuilderè mutabile, il che significa che puoi modificarne il contenuto
+         * senza creare ogni volta un nuovo oggetto. Ecco alcune delle caratteristiche chiave della StringBuilderclasse in Java:
+         *
+         * */
+        StringBuilder str=new StringBuilder("andrea");
+        System.out.println(str.reverse());
+
+    }
+}}
